c94b341c6b94bed1ae074284bc76e34548f615ec
Maastqz <127257996+Maastqz@users.noreply.github.com>
2024-06-12 11:41:08 -0300

    Add files via upload

---
+++ exercicio2.js
@@ -0,0 +1,16 @@
+//let nomes = [ana, eloisa, kaio, luna, chico, taylor, josi, agatha]
+
+//let escolha = "inicio"
+
+//function removerElementos(arr, nome, e){
+    //if(e == "inicio") {
+        
+        //arr.push(nome)
+        //console.log (arr)
+    //} else{
+       // arr.unshift(nome)
+       // console.log (arr)
+   // }
+//} 
+
+//removerElementos(nomes, maria, escolha)
